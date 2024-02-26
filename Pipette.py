07702e9eff9ea86b93611d4a8be890706fa058c8
Lilian-Deceuninck-Cappelaere <148322779+Lilian-Deceuninck-Cappelaere@users.noreply.github.com>
2024-02-26 22:38:10 +0100

    Update Pipette.py

--- Pipette.py
+++ Pipette.py
@@ -2,11 +2,13 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import numpy as np
 
-def pipette(x, y):
+def pipette(x, y, d):
     """
     Permet de savoir les couleurs RVB d'un pixel aux coordonnées x et y
     """
-    image_entrée = Image.open("image_temporaire.png")
+    image_entrée = Image.open("image_temporaire_{d - 1}.png")
     image_np = np.asarray(image_entrée)
     couleur = image_np[y, x, :]
-    print(couleur)
+    r, v, b, a = couleur
+    nouveau_texte = "\nCouleur selectionnée\nR : " + str(r) + "\nV : " + str(v) + "\nB : " + str(b) + "\nA : " + str(a)
+    text_pipette.config(text=nouveau_texte)
