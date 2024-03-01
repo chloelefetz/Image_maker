85ec1c06eba5aef5e3b7c24976be04d34948ac70
Lilian-Deceuninck-Cappelaere <148322779+Lilian-Deceuninck-Cappelaere@users.noreply.github.com>
2024-03-01 20:54:56 +0100

    Update

--- collage.py
+++ collage.py
@@ -1,53 +1,182 @@
-from PIL import Image, ImageTk, ImageDraw, ImageFilter
+from PIL import Image, ImageTk
 import tkinter as tk
-from Ouvrir_image2 import *
+from globale import zoom
+from tkinter import filedialog
+from Info_image import info_image
+
+def choix_tampons(d):
+    file_path = filedialog.askopenfilename(initialdir="tampons/")
+    d.choix = str(file_path)
+
+def boite_tampons(canvas, d):
+    """
+    Créé une fenêtre "Symetrie verticale" avec un bouton valider qui lance la fonction symetrie_verticale
+    Cette fenêtre contient un exemple de l'utilité de la fonction
+    """
+
+    # création d'une nouvelle fenetre
+    fenetre_collage = tk.Toplevel()
+    fenetre_collage.title("Collage")
+    fenetre_collage.resizable(width=False, height=False)
+    fenetre_collage.geometry("560x280+50+650")
+    fenetre_collage.attributes('-topmost', True)
+
+    # choix du tampon
+    text_tampon = tk.Label(fenetre_collage, text="1) Choissisez votre tampon :")
+    text_tampon.pack()
+    choix = tk.StringVar()
+    choix.set(f"{d.choix}")
+    text = tk.Label(fenetre_collage, textvariable=choix)
+    text.pack()
+    bouton_choix = tk.Button(fenetre_collage, text="choix", command=lambda:(choix_tampons(d), choix.set(f"{d.choix}")))
+    bouton_choix.pack()
+
+    # choix taille du tampon
+
+    text_longeur = tk.Label(fenetre_collage, text="2) Choissisez la longeur de votre tampon : ")
+    text_longeur.pack()
+    longeur = tk.Entry(fenetre_collage)
+    longeur.pack()
+    a = tk.Label(fenetre_collage, text="Choissisez la largeur de votre tampon :")
+    a.pack()    
+    largeur = tk.Entry(fenetre_collage)
+    largeur.pack() 
+    # text_emplacement = tk.Label(fenetre_collage, text="3) Choissisez la position du tampon en effectuant un click gauche de la souris sur l'image : ")
+    # text_emplacement.pack()
+
+    # Création d'une frame 
+    frame1 = tk.Frame(fenetre_collage)
+    frame1.pack(padx=3, pady=3)
+    img1 = Image.open("image_logitiel\clic_gauche.png").convert("RGBA")
+    img_exemple1 = ImageTk.PhotoImage(img1)
+    # Créer un label pour afficher l'image du clic gauche
+    image_label1 = tk.Label(frame1, image=img_exemple1)
+    image_label1.pack(side=tk.LEFT, padx=3, pady=3)
+    # Créer la zone de texte
+    texte1 = "3) Choissisez la position du tampon en effectuant un click gauche de la souris sur l'image "
+    texte_label1 = tk.Label(frame1, text=texte1)
+    texte_label1.pack(side=tk.LEFT, padx=3, pady=3)
 
 
-def collage(canvas):
+    # Création d'un bouton valider
+    bouton_valider = tk.Button(fenetre_collage, text="Valider", command=lambda:(tampons(canvas, d, longeur, largeur), fenetre_collage.destroy()))
+    bouton_valider.pack()
+    fenetre_collage.mainloop()
+
+    # Charger une image d'exemple
+    # img = Image.open("image_logitiel\chiens_sym_verticale.png").convert("RGBA")
+    # img_exemple = ImageTk.PhotoImage(img)
+
+    # Créer un label pour afficher l'image exemple
+    # image_label = tk.Label(fenetre_collage, image=img_exemple)
+    # image_label.pack()
+    
+
+
+def tampons(canvas, d, longeur, largeur):
     """
     
     """
-
     global photo
-    # Charge l'image ouverte par la fonction ouvrir_image et l'image ouverte par la fonction ouvrir_image2
-    image_entrée = Image.open("image_temporaire2.png")
-    image_sortie = image_entrée.copy()
-    image_collé =  Image.open("image_temporaire3.png")
-    image_sortie.paste(image_collé)
-    image_sortie.show
-    image_sortie.save("image_temporaire2.png")
-    # mask_im = Image.new("L", image_collé.size, 0)
-    # draw = ImageDraw.Draw(mask_im)
-    # draw.ellipse((30, 20, 500, 500), fill=255)
-    # mask_im.save('mask_circle.jpg')
+    # Charge l'image ouverte par la fonction ouvrir_image
+    image_entrée = Image.open(f"temporaire\image_temporaire_{d.indice_temp - 1}.png")
 
-    # mask_im_blur = mask_im.filter(ImageFilter.GaussianBlur(10))
-    # mask_im_blur.save('mask_circle_blur.jpg')
-    
-    
+    image2 = Image.open(d.choix).convert("RGBA")
+    image = image_entrée.convert("RGBA")
+    image2 = image2.resize((int(longeur.get()),int(largeur.get())))
+
+
+    image.alpha_composite(image2, dest=(d.x1 - image2.width // 2, d.y1 - image2.height // 2))
 
     # Sauvegarde les images pour pouvoir les afficher
-    
-    # photo = ImageTk.PhotoImage(file="image_temporaire.png")
-    # largeur_image = photo.width()
-    # hauteur_image = photo.height()
-    # canvas.config(width=largeur_image, height=hauteur_image)
-    # canvas.create_image(0, 0, anchor=tk.NW, image=photo)
+    image.save(f"temporaire\image_temporaire_{d.indice_temp}.png")
+    image_entrée = Image.open(f"temporaire\image_temporaire_{d.indice_temp}.png")
+
+    # grille de transparence image png
+    grille = Image.open('image_logitiel\grille.png')
+    image_entrée = image_entrée.convert("RGBA")
+    grille = grille.convert("RGBA").resize(image_entrée.size)
+
+    # Superposer l'image sur la grille
+    image_entrée = Image.alpha_composite(grille, image_entrée)
+
+    canvas.delete("all")
+    largeur_image = int(image_entrée.width*zoom[0])
+    hauteur_image = int(image_entrée.height*zoom[0])
+    resized_image = image_entrée.resize((largeur_image, hauteur_image), Image.Resampling.LANCZOS)
+    photo = ImageTk.PhotoImage(resized_image)
+    canvas.config(width=largeur_image, height=hauteur_image, borderwidth=0, highlightthickness=0)
+    canvas.create_image(0, 0, anchor=tk.NW, image=photo)
+    d.indice_temp += 1
 
 
 
-def boite_image2(canvas):
+
+def choix_fond(d):
+    file_path = filedialog.askopenfilename(initialdir="fond/")
+    d.choix = str(file_path)
+
+def boite_fond(canvas, d):
     """
-    Création une nouvelle page tkinter
-    bouton importer une image 
-    """ 
-    # Création d'une nouvelle fenetre tkinter
-    fenetre_import = tk.Tk()
-    fenetre_import.title("nouvelle image")
+    Créé une fenêtre "Symetrie verticale" avec un bouton valider qui lance la fonction symetrie_verticale
+    Cette fenêtre contient un exemple de l'utilité de la fonction
+    """
 
-    # Créé un bouton pour importer une image
-    button_import = tk.Button(fenetre_import, text="Importer l'image n°1", command=lambda:ouvrir_image2(canvas))
-    button_import.pack()
-    button_import = tk.Button(fenetre_import, text="Importer l'image n°2", command=lambda:ouvrir_image3(canvas))
-    button_import.pack()
-    fenetre_import.mainloop() 
+    # création d'une nouvelle fenetre
+    fenetre_collage = tk.Toplevel()
+    fenetre_collage.title("Collage")
+    fenetre_collage.resizable(width=False, height=False)
+    fenetre_collage.geometry("550x170+50+650")
+    fenetre_collage.attributes('-topmost', True)
+
+    # choix du fond
+    text_fond = tk.Label(fenetre_collage, text="1) Choissisez votre fond :")
+    text_fond.pack()
+    choix = tk.StringVar()
+    choix.set(None)
+    text = tk.Label(fenetre_collage, textvariable=choix)
+    text.pack()
+    bouton_choix = tk.Button(fenetre_collage, text="choix", command=lambda:(choix_fond(d), choix.set(f"{d.choix}")))
+    bouton_choix.pack()
+
+
+    # Création d'un bouton valider
+    bouton_valider = tk.Button(fenetre_collage, text="Valider", command=lambda:(fond(canvas, d), fenetre_collage.destroy()))
+    bouton_valider.pack()
+    fenetre_collage.mainloop()
+
+    # Charger une image d'exemple
+    # img = Image.open("image_logitiel\chiens_sym_verticale.png").convert("RGBA")
+    # img_exemple = ImageTk.PhotoImage(img)
+
+    # Créer un label pour afficher l'image exemple
+    # image_label = tk.Label(fenetre_collage, image=img_exemple)
+    # image_label.pack()
+    
+
+
+def fond(canvas, d):
+    """
+    
+    """
+    global photo
+    # Charge l'image ouverte par la fonction ouvrir_image
+    image_entrée = Image.open(f"temporaire\image_temporaire_{d.indice_temp - 1}.png")
+
+    image2 = Image.open(f"{d.choix}")
+    image_entrée = image_entrée.convert("RGBA")
+    image2 = image2.convert("RGBA").resize(image_entrée.size)
+    image = Image.alpha_composite(image2, image_entrée)
+
+    # Sauvegarde les images pour pouvoir les afficher
+    image.save(f"temporaire\image_temporaire_{d.indice_temp}.png")
+    image_entrée = Image.open(f"temporaire\image_temporaire_{d.indice_temp}.png")
+
+    canvas.delete("all")
+    largeur_image = int(image_entrée.width*zoom[0])
+    hauteur_image = int(image_entrée.height*zoom[0])
+    resized_image = image_entrée.resize((largeur_image, hauteur_image), Image.Resampling.LANCZOS)
+    photo = ImageTk.PhotoImage(resized_image)
+    canvas.config(width=largeur_image, height=hauteur_image, borderwidth=0, highlightthickness=0)
+    canvas.create_image(0, 0, anchor=tk.NW, image=photo)
+    d.indice_temp += 1
--- main.py
+++ main.py
@@ -1,40 +1,52 @@
+from globale import zoom
 import tkinter as tk 
-from PIL import Image, ImageTk
 import numpy as np
 from Ouvrir_image import ouvrir_image
+from zoom import on_entry_change
 from sauvegarder import sauvegarder_image
 from Luminosité import boite_luminosite
+from Contraste import boite_contraste
 from filtres import *
-from Rogner import rogner_image
+from position import *
+from Rogner import boite_rogner_image
 from Redimentionner import boite_redimentionner
 from pixeliser import boite_pixeliser
 from Pipette import pipette
-# from collage import boite_image2
+from transparence import boite_transparence
+from Info_image import info_image
+from collage import boite_tampons, boite_fond
 from Retour import *
 from tkinter import *
-# from Rotation import rotation
+from Rotation import rotation
 # from Theme_image import theme_image
+from os import remove, path
+from PIL import Image, ImageTk
 
-x1 = None
-y1 = None
-x2 = None
-y2 = None
+for i in range(10000):
+    if path.isfile(f"temporaire\image_temporaire_{i}.png"):
+        remove(f"temporaire\image_temporaire_{i}.png")
 
-def click_gauche_canvas(event):
+d = info_image()
+
+def click_gauche_canvas(event, text_selectionG, d):
     """
     Récupère les coordonnées x, y de l'endroit où on à cliqué avec le clique gauche
     """
-    global x1, y1
-    x1 = event.x
-    y1 = event.y
+    d.x1 = int(event.x/zoom[0])
+    d.y1 = int(event.y/zoom[0])
+    pipette(d.x1, d.y1, text_pipette, d)
+    nouveau_texte = "\nCoordonnées clique gauche\nx : " + str(d.x1) + "/ y : " + str(d.y1)
+    text_selectionG.config(text=nouveau_texte)
 
-def click_droit_canvas(event):
+def click_droit_canvas(event, text_selectionD, d):
     """
     Récupère les coordonnées x, y de l'endroit où on à cliqué avec le clique droit
     """
-    global x2, y2
-    x2 = event.x
-    y2 = event.y
+    d.x2 = int(event.x/zoom[0])
+    d.y2 = int(event.y/zoom[0])
+    pipette(d.x2, d.y2, text_pipette, d)
+    nouveau_texte = "\nCoordonnées clique droit\nx : " + str(d.x2) + "/ y : " + str(d.y2)
+    text_selectionD.config(text=nouveau_texte)
 
 
 # TEST
@@ -63,28 +75,43 @@ def test_image():
 window = tk.Tk()
 window.title("Application")
 window.geometry("1024x768")
-window.iconbitmap("logo.ico")
+window.iconbitmap("image_logitiel\logo.ico")
 window.config(background="black")
 
 #Création frame barre d'outils
 frame_outil = tk.Frame(window, bg="gray", bd=1)
 frame_outil.pack(side="left", fill="y")
 
+#Création frame informations
+frame_infos = tk.Frame(window, bg="gray", bd=1, width=20)
+frame_infos.pack(side="right", fill="y")
+
 #Ajouter du texte
 text_outil = tk.Label(frame_outil, text = "Barre d'outils", font=("Arial", 10), fg="black")
 text_outil.pack()
+text_infos = tk.Label(frame_infos, text = "Informations", font=("Arial", 10), fg="black")
+text_infos.pack()
+text_resolution = tk.Label(frame_infos, text = "\nRésolution\nLargeur : nd\nHauteur : nd", font=("Arial", 10), fg="black", width=20)
+text_resolution.pack()
+text_selectionG = tk.Label(frame_infos, text = "\nCoordonnées clique gauche\nx : nd / y : nd", font=("Arial", 10), fg="black", width=20)
+text_selectionG.pack()
+text_selectionD = tk.Label(frame_infos, text = "\nCoordonnées clique droit\nx : nd / y : nd", font=("Arial", 10), fg="black", width=20)
+text_selectionD.pack()
+text_pipette = tk.Label(frame_infos, text = "\nCouleur selectionnée\nR : nd\nV : nd\nB : nd\nA : nd", font=("Arial", 10), fg="black", width=20)
+text_pipette.pack()
+
 
 #Création d'une image vierge de taille 300x300px
 width = 300
 height = 300
-canvas = tk.Canvas(window, width=width, height=height)
+canvas = tk.Canvas(window, width=width, height=height, borderwidth=0, highlightthickness=0)
 canvas.pack()
 photo = ImageTk.PhotoImage(file="vierge.png")
 canvas.create_image(0, 0, anchor=tk.NW, image=photo)
 
 # Associe des actions aux evenement clique bouton gauche et bouton droit
-canvas.bind("<Button-1>", click_gauche_canvas)
-canvas.bind("<Button-3>", click_droit_canvas)
+canvas.bind("<Button-1>", lambda event:click_gauche_canvas(event, text_selectionG, d))
+canvas.bind("<Button-3>", lambda event:click_droit_canvas(event, text_selectionD, d))
 
 # création de l'indice pour la sauvegarde
 # indice_temp = 0
@@ -97,10 +124,8 @@ menuFichier  = Menu(menuBar, tearoff = 0)
 menuBar.add_cascade(label="Fichier",menu=menuFichier) 
 menuCouleur  = Menu(menuBar, tearoff = 0) 
 menuBar.add_cascade(label="Couleur",menu=menuCouleur)
-menuLuminosité  = Menu(menuBar, tearoff = 0) 
-menuBar.add_cascade(label="Luminosité",menu=menuLuminosité)
-menuContraste  = Menu(menuBar, tearoff = 0) 
-menuBar.add_cascade(label="Contraste",menu=menuContraste)
+menuControleImage = Menu(menuBar, tearoff = 0) 
+menuBar.add_cascade(label="Contrôle d'image",menu=menuControleImage)
 menuCollage  = Menu(menuBar, tearoff = 0) 
 menuBar.add_cascade(label="Collage",menu=menuCollage)
 menuPixeliser  = Menu(menuBar, tearoff = 0) 
@@ -109,45 +134,65 @@ menuPosition  = Menu(menuBar, tearoff = 0)
 menuBar.add_cascade(label="Position",menu=menuPosition)
 
 # Création des sous menus du menu
-menuFichier.add_command(label="Ouvrir image", command=lambda:ouvrir_image(canvas)) 
-menuFichier.add_command(label="Sauvegarder", command=sauvegarder_image) 
+menuFichier.add_command(label="Ouvrir image", command=lambda:ouvrir_image(canvas, text_resolution, slider, d))
+menuFichier.add_command(label="Sauvegarder", command=lambda:sauvegarder_image(canvas, d)) 
 menuFichier.add_command(label="Quitter", command=quit) 
-menuCouleur.add_command(label="Rouge") 
-menuCouleur.add_command(label="Bleu") 
-menuCouleur.add_command(label="Vert") 
-menuCouleur.add_command(label="Inverser Couleurs", command=lambda:inverser_couleur_image(canvas)) 
-menuLuminosité.add_command(label="Luminosité", command=lambda:boite_luminosite(canvas))
-menuPixeliser.add_command(label="Pixeliser", command=lambda:boite_pixeliser(canvas))
-# menuPosition.add_command(label="Rotation",  command=lambda:rotation(canvas)) 
-menuPosition.add_command(label="Miroir", command=lambda:miroir(canvas))
-menuPosition.add_command(label="Mirroir central", command=lambda:miroir_central(canvas)) 
-menuPosition.add_command(label="Miroir vertical", command=lambda:miroir_vertical(canvas))
-menuPosition.add_command(label="Miroir horizontal", command=lambda:miroir_horizontal(canvas))
- 
+menuCouleur.add_command(label="Rouge", command=lambda:boite_rouge(canvas, d))
+menuCouleur.add_command(label="Vert", command=lambda:boite_verte(canvas, d))
+menuCouleur.add_command(label="Bleu", command=lambda:boite_bleu(canvas, d))
+menuCouleur.add_command(label="cyan", command=lambda:boite_cyan(canvas, d))
+menuCouleur.add_command(label="jaune", command=lambda:boite_jaune(canvas, d))
+menuCouleur.add_command(label="magenta", command=lambda:boite_magenta(canvas, d))
+menuCouleur.add_command(label="Inverser Couleurs", command=lambda:boite_inverser_couleur(canvas, d))
+menuCouleur.add_command(label="Noir et Blanc", command=lambda:boite_gris(canvas, d))
+menuCouleur.add_command(label="Sépia", command=lambda:boite_sepia(canvas, d))
+menuControleImage.add_command(label="Luminosité", command=lambda:boite_luminosite(canvas, d))
+menuControleImage.add_command(label="Contraste", command=lambda:boite_contraste(canvas, d))
+menuControleImage.add_command(label="Transparence", command=lambda:boite_transparence(canvas, d))
+menuCollage.add_command(label="Tampon", command=lambda:boite_tampons(canvas, d))
+menuCollage.add_command(label="Fond", command=lambda:boite_fond(canvas, d))
+menuPixeliser.add_command(label="Pixeliser", command=lambda:boite_pixeliser(canvas, d))
+menuPosition.add_command(label="Rotation",  command=lambda:rotation(canvas, d)) 
+menuPosition.add_command(label="Miroir vertical", command=lambda:boite_miroir_vertical(canvas, d))
+menuPosition.add_command(label="Miroir horizontal", command=lambda:boite_miroir_horizontal(canvas, d))
+menuPosition.add_command(label="Symetrie central", command=lambda:boite_symetrie_centrale(canvas, d))
+menuPosition.add_command(label="Symetrie vertical", command=lambda:boite_symetrie_verticale(canvas, d))
+menuPosition.add_command(label="Symetrie horizontal", command=lambda:boite_symetrie_horizontale(canvas, d))
+
 # Configuration de la barre des menus
 window.config(menu=menuBar)
 
 #Créations boutons 
-button_filtre = tk.Button(frame_outil, text="Inverser les couleurs", command=lambda:inverser_couleur_image(canvas))
-button_filtre.pack()
-# button_filtre = tk.Button(frame_outil, text="filtre rouge", command=lambda:rouge_image(canvas))
-# button_filtre.pack()
-button_rogner = tk.Button(frame_outil, text="Rogner", command=lambda:rogner_image(canvas))
+image_retour = Image.open("fleche_retour.png")
+photo_retour = ImageTk.PhotoImage(image_retour)
+button_collage = tk.Button(frame_outil, text = " Retour", image=photo_retour, compound=tk.LEFT, width=120, command=lambda:retour_arriere(canvas, d))
+button_collage.pack()
+image_retour_debut = Image.open("fleche_retour_debut.png")
+photo_retour_debut = ImageTk.PhotoImage(image_retour_debut)
+button_collage = tk.Button(frame_outil, text = " Retour début", image=photo_retour_debut, compound=tk.LEFT, width=120, command=lambda:retour_debut(canvas, d))
+button_collage.pack()
+image_rogner = Image.open("rogner.png")
+photo_rogner = ImageTk.PhotoImage(image_rogner)
+button_rogner = tk.Button(frame_outil, text = " Rogner", image=photo_rogner, compound=tk.LEFT, width=120, command=lambda:boite_rogner_image(canvas, d))
 button_rogner.pack()
-button_redimentionner = tk.Button(frame_outil, text="Redimensionner", command=lambda:boite_redimentionner(canvas))
+image_redimentionner = Image.open("redimentionner.png")
+photo_redimentionner = ImageTk.PhotoImage(image_redimentionner)
+button_redimentionner = tk.Button(frame_outil, text = " Redimensionner", image=photo_redimentionner, compound=tk.LEFT, width=120, command=lambda:boite_redimentionner(canvas, text_resolution, d))
 button_redimentionner.pack()
-button_pipette = tk.Button(frame_outil, text="Pipette", command=lambda:pipette(x1, y1))
-button_pipette.pack()
+# button_pipette = tk.Button(frame_outil, text="Pipette", command=lambda:pipette(x1, y1, text_pipette))
+# button_pipette.pack()
 # button_collage = tk.Button(frame_outil, text="Collage", command=lambda:boite_image2(canvas))
 # button_collage.pack()
-button_collage = tk.Button(frame_outil, text="<-", command=lambda:retour_arriere(canvas))
-button_collage.pack()
-button_collage = tk.Button(frame_outil, text="->", command=lambda:retour_avant(canvas))
-button_collage.pack()
+ 
+slider = tk.Scale(frame_outil, from_=10, to=200, orient="horizontal")
+slider.pack(padx=10, pady=10, side="bottom")
+label_zoom = tk.Label(frame_outil, text="Zoom")
+label_zoom.pack(side="bottom")
+initial_value = 100
+slider.set(initial_value)
+slider.bind("<ButtonRelease-1>", lambda event: on_entry_change(event, canvas, d)) # <ButtonRelease-1> = l'utilisateur relache le bouton 1
 
-button_test = tk.Button(frame_outil, text="TEST", command=test_image)
-button_test.pack()
 
-
-
+# button_test = tk.Button(frame_outil, text="TEST", command=lambda:test_zoom(canvas))
+# button_test.pack()
 window.mainloop()
