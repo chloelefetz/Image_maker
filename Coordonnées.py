c2e1d9210cde22043d72fdeb0ef74011c948a9d5
Lilian-Deceuninck-Cappelaere <148322779+Lilian-Deceuninck-Cappelaere@users.noreply.github.com>
2024-03-05 18:42:39 +0100

    Delete Coordonnées.py

--- Coordonnées.py
+++
@@ -1,9 +0,0 @@
-from Pipette import pipette
-
-class Coordonnées(object):
-    def __init__(self) -> None:
-        self.x1 = None
-        self.x2 = None
-
-        self.y1 = None
-        self.y2 = None
