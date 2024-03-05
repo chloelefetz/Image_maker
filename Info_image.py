b0d487299451fdd5ddcb9746c49373d3e174652b
Lilian-Deceuninck-Cappelaere <148322779+Lilian-Deceuninck-Cappelaere@users.noreply.github.com>
2024-03-05 18:37:45 +0100

    Create Info_image.py

---
+++ Info_image.py
@@ -0,0 +1,11 @@
+class info_image(object):
+    def __init__(self) -> None:
+        self.x1 = None
+        self.x2 = None
+
+        self.y1 = None
+        self.y2 = None
+
+        self.choix = None
+
+        self.indice_temp = 0
